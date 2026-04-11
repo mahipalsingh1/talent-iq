23ce457e5361ae2b80f6483ed3b37ef14f68c6d3
mahipalsingh1 <bhanwarmahipals@gmail.com>
2026-04-11 12:35:42 +0530

    final commits

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,20 +1,8 @@
 import axios from "axios";
-import { getToken } from "@clerk/clerk-react";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
-});
-
-// ✅ ADD AUTH TOKEN IN EVERY REQUEST
-axiosInstance.interceptors.request.use(async (config) => {
-  const token = await getToken();
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
 });
 
 export default axiosInstance;
